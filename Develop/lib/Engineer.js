7bc1ffbd4915bdb573d343d7f7ab9805ab282c43
Rooster <othmanmuhidean@gmail.com>
2020-04-17 13:51:34 -0700

    Engineer constructor has passed all tests

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,3 +1,16 @@
-class Engineer{
-    constructor()
+const Employee = require("./Employee.js");
+
+class Engineer extends Employee {
+  constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+  }
+  getRole() {
+    return "Engineer";
+  }
+  getGithub() {
+    return this.github;
+  }
 }
+
+module.exports = Engineer;
