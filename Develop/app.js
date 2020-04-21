09cb6e12c4d5f21141247d8a0b0588a17d0a59fd
Rooster <othmanmuhidean@gmail.com>
2020-04-21 00:08:47 -0700

    I have inquirer set up with questions and validation.

--- Develop/app.js
+++ Develop/app.js
@@ -24,6 +24,34 @@ const questions = [
       return "Please enter your first or last name";
     },
   },
+  {
+    name: "id",
+    type: "input",
+    message: "Please enter in a valid ID Number",
+    when: function (answers) {
+      return answers.name;
+    },
+    validate: (input) => {
+      if (input === NaN || input.length < 6) {
+        return "You must enter in 6 digits";
+      }
+      return true;
+    },
+  },
+  {
+    name: "email",
+    type: "input",
+    message: "Please enter in a valid email address",
+    when: function (answers) {
+      return answers.id;
+    },
+    validate: (input) => {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+        return true;
+      }
+      return "You have entered an invalid email address!";
+    },
+  },
 ];
 prompt(questions).then((answers) => {
   console.log(answers.welcome);
